896ff09fb56ef0e97b051359a1ccb716c2362e4d
ShauryaSingh234 <shauryasinghrk@gmail.com>
2026-05-07 18:48:56 +0530

    Delete Labst1.jsx

--- Labst1.jsx
+++
@@ -1,8 +0,0 @@
-function start(){
-    let n = document.getElementById("userInput").value;
-    let output = "";
-    for (let i = n; i >= 1; i--){
-        output += i + "<br>";
-    }
-    document.getElementById("output").innerHTML = output;
-}
